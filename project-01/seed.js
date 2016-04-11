e39c57d0dd7c9248481e0c89b60c077ad53e2ee3
andreaainara <andrea.ainara10@gmail.com>
2016-04-11 09:54:55 -0700

    seriously i hate handlebars

--- project-01/seed.js
+++ project-01/seed.js
@@ -24,6 +24,8 @@ db.Exchange.remove({}, function(err, exchanges){
     if (err) { return console.log('ERROR', err); }
     console.log("all exchanges:", exchanges);
     console.log("created", exchanges.length, "exchanges");
+    // console.log(exchanges[0].book)
+    console.log(exchanges[0].book)
     process.exit();
   });
 });
