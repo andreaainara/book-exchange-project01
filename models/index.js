c350e9f3d101ce6b2c3c003c62036b74bf68a179
andreaainara <andrea.ainara10@gmail.com>
2016-04-15 20:25:56 -0700

    trying to fix heroku deployment

--- models/index.js
+++ models/index.js
@@ -1,5 +1,9 @@
 var mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost/book-exchange");
+mongoose.connect(process.env.MONGOLAB_URI ||
+    process.env.MONGOHQ_URL ||
+    "mongodb://heroku_c6d1p9dn:9156ijve82jiceehe8m2vl4lfc@ds023550.mlab.com:23550/heroku_c6d1p9dn");
+
+
 
 var Exchange = require('./exchange');
 
